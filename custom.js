c9b8dbd1f1319ff0defd733b2134a112bfc0abf0
Desiree Wildgrube <145672131+DayzWild@users.noreply.github.com>
2024-03-27 10:01:41 +0100

    added project even fibonacci numbers
    Function gets the sum of all even-valued numbers of the Fibonacci sequence

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector("#btn");
 const solution = document.querySelector("#solution");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function() {
 
     let sum = 0;
 
@@ -16,5 +16,36 @@ btn.addEventListener("click", function () {
     }
 
     solution.textContent = sum;
+
+});
+
+
+
+const btn2 = document.querySelector("#btn2");
+const solution2 = document.querySelector("#solution2");
+
+btn2.addEventListener("click", function() {
+
+    let a = 1;
+    let b = 2;
+    let sum = b;
+    let c = a + b;
+
+    for (i = 1; c <= 4000000; i++) {
+
+        a = b;
+        b = c;
+        c = a + b
+
+        if (b % 2 == 0) {
+
+            sum += b;
+
+        }
+
+    }
+
+    solution2.textContent = solution2.textContent.slice(0, -2);
+    solution2.textContent = sum;
 
 });
